668c5a5414a370c1ef3e3b9fad76c2f45515bd05
Danielle Starlet <dlloyd20@ivytech.edu>
2024-09-19 13:04:22 -0400

    add_new_media method created

--- Media_Class.py
+++ Media_Class.py
@@ -9,7 +9,7 @@ class Media:
         self.author = author
         self.serial_num = self.generate_serial_num() #function to generate serial number
         self.in_stock = in_stock
-        self.Tags = tags                             
+        self.tags = tags                             
 
     # Generates 8 digit serial number    
     def generate_serial_num(self, length=8):
@@ -26,3 +26,22 @@ class Media:
             f"In Stock: {'Yes' if self.in_stock else 'No'}\n"
             f"Tags: {', '.join(f'{key}: {value}' for key, value in self.tags.items())}"
         )
+
+    # Method to add new media items    
+    @classmethod
+    def add_new_media(cls):
+        name = input("Enter the name of the media: ")
+        media_type = input("Enter the type of media (Book, Magazine, CD, DVD): ")
+        genre = input("Enter the main genre of the media: ")
+        author = input("Enter the author of the media: ")
+        in_stock = input("Is the media in stock? (yes/no): ").lower() == 'yes' #not sure if this redundent or not. if were adding wouldn't it be in stock?
+        tags = {}
+        while True:
+            key = input("Enter a tag key (or 'done' to finish): ")
+            if key.lower() == 'done':
+                break
+            value = input(f"Enter the value for tag '{key}': ")
+            tags[key] = value
+        # not sure what exactly we will add here. Maybe things like condition: new or cut: Director's
+        # key will store the first, value stores the second.
+        return cls(name, media_type, genre, author, in_stock, tags)
