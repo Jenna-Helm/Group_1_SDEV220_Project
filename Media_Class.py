8d837ab022ee219d722b4caf8a21acb01bcea6f1
Danielle Starlet <dlloyd20@ivytech.edu>
2024-09-21 19:40:03 -0400

    created unique id as the generated id. changed serial number to an input

--- Media_Class.py
+++ Media_Class.py
@@ -4,17 +4,19 @@ import tkinter as tk
 from tkinter import ttk
 
 class Media:
-    def __init__(self, name, media_type, genre, author, in_stock, tags):
+    def __init__(self, name, media_type, genre, author, serial_num, in_stock, tags):
         self.name = name
         self.media_type = media_type #Book, Magazine, CD, DVD, etc.
         self.genre = genre
         self.author = author
-        self.serial_num = self.generate_serial_num() #function to generate serial number
+        self.unique_id = self.generate_unique_id() #function to generate unique id
+        self.serial_num= serial_num
         self.in_stock = in_stock
         self.tags = tags                             
 
-    # Generates 8 digit serial number    
-    def generate_serial_num(self, length=8):
+    # Generates 5 digit unique id   
+    @staticmethod
+    def generate_unique_id(length=5):
         return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
     
     # Prints report of media item
@@ -24,9 +26,10 @@ class Media:
             f"Type: {self.media_type}\n"
             f"Genre: {self.genre}\n"
             f"Author: {self.author}\n"
+            f"ID: {self.unique_id}\n"
             f"Serial Number: {self.serial_num}\n"
             f"In Stock: {'Yes' if self.in_stock else 'No'}\n"
-            f"Tags: {', '.join(f'{key}: {value}' for key, value in self.tags.items())}"
+            f"Tags: {', '.join(self.tags)}"
         )
 
     # Method to add new media items    
@@ -36,18 +39,14 @@ class Media:
         media_type = input("Enter the type of media (Book, Magazine, CD, DVD): ")
         genre = input("Enter the main genre of the media: ")
         author = input("Enter the author of the media: ")
-        in_stock = input("Is the media in stock? (yes/no): ").lower() == 'yes' #not sure if this redundent or not. if were adding wouldn't it be in stock?
-        tags = {}
-        while True:
-            key = input("Enter a tag key (or 'done' to finish): ")
-            if key.lower() == 'done':
-                break
-            value = input(f"Enter the value for tag '{key}': ")
-            tags[key] = value
-        # not sure what exactly we will add here. Maybe things like condition: new or cut: Director's
-        # key will store the first, value stores the second.
-        return cls(name, media_type, genre, author, in_stock, tags)
+        serial_num = input("Enter the serial number of the media: ")
+        in_stock = input("Is the media in stock? (yes/no): ").lower() == 'yes'
+        tags = input("Enter tags for the media (comma-separated): ").split(',')
+        tags = [tag.strip() for tag in tags]  # Remove any extra whitespace
     
-
+        # Generate unique_id when adding new media
+        unique_id = cls.generate_unique_id()
+    
+        return cls(name, media_type, genre, author, unique_id, serial_num, in_stock, tags)
 
 
