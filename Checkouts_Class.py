4a39f0a7cef755acbb2cd94fc7d6a7203d78c241
zachary rosch <zrosch@ivytech.edu>
2024-09-25 14:03:21 -0400

    Continued work on Checkouts.

--- Checkouts_Class.py
+++ Checkouts_Class.py
@@ -24,6 +24,7 @@ class CheckoutNew(tk.Tk):
         checkNameInput = Text(
             self,
             height=1,
+            width=20,
             relief="raised"
         )
         checkNameInput.grid(column=1,row=0)
@@ -31,8 +32,28 @@ class CheckoutNew(tk.Tk):
 
         #create a seperator
         separator = ttk.Separator(self, orient='horizontal')
-        separator.grid(column=1,row=2,columnspan=3,ipady=5)
+        separator.grid(column=0,row=1,rowspan=1,columnspan=2,ipady=2)
 
+        #create an add button. used to add a new item to the current checkout
+        add_fld = Text(
+            self,
+            height=1,
+            width=25
+        )
+        add_fld.grid(column=0,columnspan=2,row=4)
+        
+        add_butn = tk.Button(
+            text="Add",
+            relief="raised"
+        )
+        add_butn.grid(column=3,row=4)
+        
+        add_labl = tk.Label(
+            self,
+            text="Add Serial",
+            relief="raised"
+        )
+        add_labl.grid(column=0,row=3,columnspan=3)
 
 
 # debug. make the window locally for rapid testing
